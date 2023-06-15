021e1132586b697c41741d55be8fc1d0365404c1
João Vitor Santos Tavares <jvst@ic.ufal.br>
2023-06-14 22:40:34 -0300

    Update typeorm.config.ts

--- api-teste-lacomp/src/configs/typeorm.config.ts
+++ api-teste-lacomp/src/configs/typeorm.config.ts
@@ -4,8 +4,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
     port: 5432,
-    username: 'postgres',
-    password: '5657',
+    username: 'yourUSer',
+    password: 'yourPassword',
     database: 'lacomp_api',
     entities: [__dirname + '/../**/*.entity.{js, ts}'],
     synchronize: true
